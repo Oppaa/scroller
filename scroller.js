987f2fd957fb2a904d0c520ca578059b7c0b3ad4
oppaa <blank114@163.com>
2014-11-29 20:43:11 +0800

    基本结构

--- scroller.js
+++ scroller.js
@@ -1,3 +1,36 @@
-;(function(){
-// ready to start
-})();
+;(function(win,D,undefined){
+
+	function scroller(){
+
+	},
+
+	function momentum(){
+		// from iscroll
+	},
+
+	scroller.prototype={
+		constructor:scroller,
+		init:function(){
+
+		},
+		handleEvent:function(){
+
+		},
+		setup:function(){
+
+		},
+		remove:function(){
+
+		},
+		html:function(){
+
+		},
+		refresh:function(){
+
+		}
+
+	}
+	
+	win.touchScroller=scroller;
+
+})(window,document);
